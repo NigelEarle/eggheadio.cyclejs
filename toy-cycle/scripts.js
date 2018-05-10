37023fa504be44c5e55905177cc7c0fe230380e4
nwre87 <nwre87@gmail.com>
2018-05-09 21:18:10 -0700

    :stuck_out_tongue_winking_eye: Add webpack because some deps aint actin right

--- toy-cycle/scripts.js
+++ toy-cycle/scripts.js
@@ -1,15 +1,26 @@
-function main() {
+import xs from 'xstream';
+import fromEvent from 'xstream/extra/fromEvent';
+
+const main = sources => {
+  const click$ = sources.DOM;
   // Logic
   return {
-    DOM: xs.periodic(1000) // stream every x ms
-      .fold(prev => prev + 1, 0)
+    DOM: click$.startWith(null)
+      .map(_ => (
+        xs.periodic(1000) // stream every x ms
+        .fold(prev => prev + 1, 0)
+      ))
+      .flatten()
       .map(i => `Seconds elapsed: ${i}`),
     log: xs.periodic(2000)
       .fold(prev => prev + 1, 0)
   }
 }
 
-function domDriver(text$) {
+// source = input effect - read
+// sink = output effect - write
+
+const domDriver = text$ => {
   // Effects
   text$.subscribe({
     next: str => {
@@ -17,9 +28,12 @@ function domDriver(text$) {
       .textContent = str
     }
   });
+
+  const domSource = fromEvent(document, 'click');
+  return domSource;
 }
 
-function logDriver(msg$) {
+const logDriver = msg$ => {
   msg$.subscribe({
     next: msg => {
       console.log(msg);
@@ -27,14 +41,20 @@ function logDriver(msg$) {
   });
 }
 
-function run(mainFn, drivers) {
-  const sinks = mainFn();
-  Object.keys(drivers)
-  .forEach(driver => {
-    if (sinks[driver]) {
-      drivers[driver](sinks[driver]);
-    }
-  });
+/*
+Solution to circular dependency
+
+fakeA = ...
+b = f(fakeA)
+a = g(b)
+fakeA.behaveLike(a)
+*/
+
+const run = (mainFn, drivers) => {
+  const fakeDOMsink = xs.create();
+  const domSource = domDriver(fakeDOMsink);
+  const sinks = mainFn({DOM: domSource})
+  fakeDOMsink.imitate(sinks.DOM);
 }
 
 run(main, {
---
+++ toy-cycle/webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  mode: 'development',
+  entry: './scripts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
