73c6d086b1a5f31bba26a1715ddaa8cd71d00b91
nwre87 <nwre87@gmail.com>
2018-05-09 18:48:50 -0700

    :car: Main and dom drivers

--- toy-cycle/scripts.js
+++ toy-cycle/scripts.js
@@ -1,9 +1,28 @@
-xs.periodic(1000) // stream every x ms
-  .fold(prev => prev + 1, 0)
-  .map(i => `Seconds elapsed: ${i}`)
-  .subscribe({
+function main() {
+  // Logic
+  return xs.periodic(1000) // stream every x ms
+    .fold(prev => prev + 1, 0)
+    .map(i => `Seconds elapsed: ${i}`)
+}
+
+function domDriver(text$) {
+  // Effects
+  text$.subscribe({
     next: str => {
       document.querySelector('#app')
       .textContent = str
     }
-  })
+  });
+}
+
+function logDriver(msg$) {
+  msg$.subscribe({
+    next: msg => {
+      console.log(msg);
+    }
+  });
+}
+
+const sink = main();
+domDriver(sink);
+logDriver(sink)
